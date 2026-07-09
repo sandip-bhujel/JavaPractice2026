04a8f2aa0924f41b8a5182be19e975067884450c
sandip-bhujel <bhujelsandip610@gmail.com>
2026-07-09 23:17:49 +0900

    "first java practice in intellij "

---
+++ src/PracticeDay01.java
@@ -0,0 +1,48 @@
+public class PracticeDay01 {
+    public static void main(String []args) {
+
+        //variables
+        String name = "saman bahadur";
+        String chimeki = "samita ";
+        int age = 30;
+        //memory location ma different different value store garna sakchau
+
+        String friend = "chimeki"; //samita bhanney name yo friend ma pani save vako cha
+
+
+        //types of variable
+        //primitive type
+        //non primitive type
+
+        //primitive type variable
+        //byte - 1 [-128 to 127] yo vittra ko number byte ma store garna sakincha
+        //short - 2 byte ko memory lincha
+        //int- 4 byte ko memory lincha  1 , 2, 3, 4
+        //long - 8 byte memory
+        //float - decimal value huncha  4   3.14
+        //double - 8
+        //char - 2
+        //boolean- 1 true or false
+
+        byte age1 = 30;
+        int phone = 12345678;
+        //12345678900  yo save garnalai long use garnu parcha
+        long phone2 = 123456789000L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = true;
+
+        //non primitive type
+        String name1 = "hari bahadur";
+        System.out.println(name1.length()); // 0 bata count huncha length ma
+
+//      System.out.println("Hello Intellij Bro!");
+    }
+
+
+
+
+
+
+}
+
