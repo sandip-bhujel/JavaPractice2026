5d41d9f1c776becc89ebb0db7731b946613729d6
sandip-bhujel <bhujelsandip610@gmail.com>
2026-07-14 23:40:11 +0900

    this is day2 of variable practice

---
+++ src/VariablesPracticeDay02.java
@@ -0,0 +1,20 @@
+public class PracticeDay02 {
+    public static void main(String[] args) {
+        //non primitive types
+//        String name = new String ("akanchya");
+//        String friend = new String("mahendra");
+////        System.out.println(name.length());
+//        System.out.println(friend.length());
+
+        //string ma cocatinate
+        String name1 = "mahesh";
+        String name2 = "samjhana";
+        String address = "jhapa nepal";
+        int age = 30;
+        String all = name1 + "and" + name2 + address + age;
+        System.out.println(all);
+
+
+    }
+
+}
